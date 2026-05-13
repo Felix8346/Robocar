533d46b568f1075af9ab6295411d44facb726c8e
Felix8346 <stuehlefs@gmail.com>
2026-05-13 17:27:02 +0200

    Update sensors.py

--- src/sensors.py
+++ src/sensors.py
@@ -1,11 +1,8 @@
-from gpiozero import LineSensor
 from signal import pause
-sensor = LineSensor (23)
-sensor.when_line = lambda: print ("line detected")
+
+from gpiozero import LineSensor
+
+sensor = LineSensor(23)
+sensor.when_line = lambda: print("line detected")
 sensor.when_no_line = lambda: print("no line detected")
-pause ()
-
-
-
-
-
+pause()
